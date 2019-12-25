50c9079c6a274a9327c43eb196ecd51b6a868e35
kwansikdev <kwansk0424@gmail.com>
2019-12-25 20:16:12 +0900

    redux_reducers edited

--- youtubeclone/src/actions/action.js
+++ youtubeclone/src/actions/action.js
@@ -1,5 +1,7 @@
 export const UPDATE_QUERY = 'UPDATE_QUERY';
 export const LIKECOUNT = 'LIKECOUNT';
+export const DISLIKECOUNT = 'DISLIKECOUNT';
+export const CHANNELINFO = 'CHANNELINFO';
 
 export function updateQuery(query) {
   return { type: UPDATE_QUERY, query };
@@ -7,4 +9,12 @@ export function updateQuery(query) {
 
 export function likeCount(id) {
   return { type: LIKECOUNT, id };
+}
+
+export function dislikeCount(id) {
+  return { type: DISLIKECOUNT, id };
+}
+
+export function selectedchannelInfo(id, channelData) {
+  return { type: CHANNELINFO, id, channelData };
 }
--- youtubeclone/src/reducers/reducer.js
+++ youtubeclone/src/reducers/reducer.js
@@ -1,4 +1,9 @@
-import { UPDATE_QUERY, LIKECOUNT } from '../actions/action';
+import {
+  UPDATE_QUERY,
+  LIKECOUNT,
+  DISLIKECOUNT,
+  CHANNELINFO,
+} from '../actions/action';
 
 const INITIONAL_STATE = {
   query: '',
@@ -6,6 +11,7 @@ const INITIONAL_STATE = {
 };
 
 export default function updateStore(state = INITIONAL_STATE, action) {
+  const video = state.data[action.id]; // action.id안의 객체
   switch (action.type) {
     case UPDATE_QUERY:
       return {
@@ -13,16 +19,39 @@ export default function updateStore(state = INITIONAL_STATE, action) {
         query: action.query,
       };
     case LIKECOUNT:
-      const video = state.data[action.id]; // action.id안의 객체
       return {
         ...state,
         data: {
           ...state.data,
           [action.id]: {
             ...video,
-            count: video // 초기상태일때의 방어코드, 초기 값에 미리 0을 써두지 않는 이유는, 해당 video id값이 랜덤이기에 video id 값안에 있는 값을 초기값으로 넣어둘 수가 없다
-              ? video.count + 1
-              : 1,
+            likeCount:
+              video && video.likeCount // 초기상태일때의 방어코드, 초기 값에 미리 0을 써두지 않는 이유는, 해당 video id값이 랜덤이기에 video id 값안에 있는 값을 초기값으로 넣어둘 수가 없다
+                ? video.likeCount + 1
+                : 1,
+          },
+        },
+      };
+    case DISLIKECOUNT:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [action.id]: {
+            ...video,
+            dislikeCount:
+              video && video.dislikeCount ? video.dislikeCount + 1 : 1,
+          },
+        },
+      };
+    case CHANNELINFO:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [action.id]: {
+            ...video,
+            channelInfo: action.channelData,
           },
         },
       };
